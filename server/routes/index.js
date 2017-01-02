947d8ea9281958cb7b9de00801d7a2967c63e84d
Tyler Briskie <tylerbriskie@gmail.com>
2017-01-02 13:31:02 -0700

    express set up, starting database seeding process

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Little Black Book' });
 });
+
+// router.get('/suitors', (req, res)=>{
+//   res.render('index', { title: 'Suitors' })
+// })
 
 module.exports = router;
