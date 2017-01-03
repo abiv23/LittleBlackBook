39d57c47801aaaf460ab5e125f1c811a035ffab7
Chris Houdlette <choudlette@gmail.com>
2017-01-03 11:03:45 -0700

    able to pull login data from the db

--- server/routes/login.js
+++ server/routes/login.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var knex = require('../db/knex.js');
 
 /* GET home page. */
 router.post('/', function(req, res, next) {
-  // res.render('index', { title: 'Login Route' });
+  let profile = req.body;
+  knex.select().table('profile').where('email', profile.email)
+  .then(data => {
+    console.log(data);
+  })
   console.log(req.body)
 
 });
