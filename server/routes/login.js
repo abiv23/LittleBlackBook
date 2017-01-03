3ab0e765ad5cef0848981888382fb79c95555ab5
Chris Houdlette <choudlette@gmail.com>
2017-01-03 13:42:57 -0700

    :sparkles: worked on password encryption and landing page styling

--- server/routes/login.js
+++ server/routes/login.js
@@ -1,13 +1,22 @@
 var express = require('express');
 var router = express.Router();
 var knex = require('../db/knex.js');
+var protect = require('../db/encryption.js');
 
+router.get('/', function(req,res,next){
+  protect.encrypt(req.query.password).then(hash => {
+  console.log(hash);
+});
+
+
+});
 /* GET home page. */
 router.post('/', function(req, res, next) {
   let profile = req.body;
   knex.select().table('profile').where('email', profile.email)
   .then(data => {
     console.log(data);
+    protect.decrypt(data.password, profile.password);
   })
   console.log(req.body)
 
