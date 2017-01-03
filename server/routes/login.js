65e42b304897a4efda7893b443ff7c7ec041d507
Chris Houdlette <choudlette@gmail.com>
2017-01-03 14:19:42 -0700

    finished nav styling

--- server/routes/login.js
+++ server/routes/login.js
@@ -3,22 +3,21 @@ var router = express.Router();
 var knex = require('../db/knex.js');
 var protect = require('../db/encryption.js');
 
-router.get('/', function(req,res,next){
-  protect.encrypt(req.query.password).then(hash => {
-  console.log(hash);
-});
+router.get('/', function(req, res, next) {
+    protect.encrypt(req.query.password).then(hash => {
+        console.log(hash);
+    });
 
 
 });
 /* GET home page. */
 router.post('/', function(req, res, next) {
-  let profile = req.body;
-  knex.select().table('profile').where('email', profile.email)
-  .then(data => {
-    console.log(data);
-    protect.decrypt(data.password, profile.password);
-  })
-  console.log(req.body)
+    let profile = req.body;
+    knex.select().table('profile').where('email', profile.email)
+        .then(data => {
+            protect.decrypt(data.password, profile.password);
+        });
+    console.log(req.body)
 
 });
 
