91ba73149551e8dea498d998cce91757be756746
“Andrew <abiv23@gmail.com>
2017-01-03 14:03:04 -0700

    :sparkles: finished all migrations and seeds

--- server/seeds/02suitors.js
+++ server/seeds/02suitors.js
@@ -7,25 +7,29 @@ exports.seed = function(knex, Promise) {
                 name: 'Candi',
                 age: '18',
                 where_met: '7-11',
-                rating: '8'
+                rating: '8',
+                id: 1
             }, {
                 profile_id: 2,
                 name: 'Douglas',
                 age: '24',
                 where_met: 'Grinder',
-                rating: '8'
+                rating: '8',
+                id: 2
             }, {
                 profile_id: 3,
                 name: 'Pete',
                 age: '33',
                 where_met: 'Broncos Game',
-                rating: '7'
+                rating: '7',
+                id: 3
             }, {
                 profile_id: 4,
                 name: 'Karen',
                 age: '28',
                 where_met: 'Brunch with Friends',
-                rating: '10'
+                rating: '10',
+                id: 4
             }]
             return knex('suitor').insert(suitors);
         });
