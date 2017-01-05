7302324da8d23723a3e8de04390b63c457aed8a4
“Andrew <abiv23@gmail.com>
2017-01-05 11:40:47 -0700

    admin routes are restful

--- routes/profile.js
+++ routes/profile.js
@@ -5,30 +5,32 @@ var protect = require('../db/encryption.js');
 
 /* GET users listing. */
 router.get('/:id', function(req, res, next) {
-  knex('profile').where('id', req.params.id).first().then(data=>{
-    res.render('profile', {data});
-  })
+    knex('profile').where('id', req.params.id).first().then(data => {
+        res.render('profile', {
+            data
+        });
+    })
 
 });
 
-router.post('/:id/update', function(req, res, next){
-  let userPassword = protect.encrypt(req.body.password).then(data=>{
-    return data;
-  });
-  console.log(userPassword);
-  knex('profile').where('id', req.params.id).update({
-    first_name: req.body.first_name,
-    last_name: req.body.last_name,
-    email: req.body.email
-  }).then(data=>{
-    res.redirect(`/profile/${req.params.id}`)
-  });
+router.post('/:id/update', function(req, res, next) {
+    let userPassword = protect.encrypt(req.body.password).then(data => {
+        return data;
+    });
+    console.log(userPassword);
+    knex('profile').where('id', req.params.id).update({
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        email: req.body.email
+    }).then(data => {
+        res.redirect(`/profile/${req.params.id}`)
+    });
 })
 
-router.post('/:id/delete', function(req,res,next) {
-  knex('profile').where('id', req.params.id).first().del().then(data=>{
-    res.redirect('/');
-  });
+router.delete('/:id/delete', function(req, res, next) {
+    knex('profile').where('id', req.params.id).first().del().then(data => {
+        res.redirect('/admin');
+    });
 });
 
 module.exports = router;
