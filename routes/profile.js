e69471afa57a45e33382a5b6ba3a1d56bb892eda
“Andrew <abiv23@gmail.com>
2017-01-05 14:35:09 -0700

    changed link src on toolkit

--- routes/profile.js
+++ routes/profile.js
@@ -13,80 +13,53 @@ router.get('/:id', function(req, res, next) {
 
 });
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 router.post('/:id/update', function(req, res, next) {
-    let userPassword = protect.encrypt(req.body.password).then(data => {
-        return data;
-    });
-    console.log(userPassword);
-    knex('profile').where('id', req.params.id).update({
-        first_name: req.body.first_name,
-        last_name: req.body.last_name,
-        email: req.body.email
-    }).then(data => {
-        res.redirect(`/profile/${req.params.id}`)
-    });
-})
+    let profile = req.body;
+    console.log(profile);
+    if (profile.password.length > 0) {
+        knex.select().table('profile').where('id', req.params.id).first()
+            .then(data => {
+                protect.decrypt(data.password, profile.password).then(result => {
+                    if (result) {
+                        console.log("password is correct")
+                        if ((profile.new_password === profile.new_password_verify) && (profile.new_password.length > 7)) {
+                            console.log("passwords match")
+                            req.body.new_password = protect.encrypt(req.body.new_password).then(data => {
+                                knex('profile').where('id', req.params.id).update({
+                                    first_name: profile.first_name,
+                                    last_name: profile.last_name,
+                                    email: profile.email,
+                                    password: data
+                                }).then(data => {
+                                    res.redirect(`/profile/${req.params.id}`);
+                                });
+                            })
+                        } else {
+                            console.log("new passwords do not match or not long enough.")
+                            res.redirect(`/profile/${req.params.id}`);
+                        }
+                    } else {
+                        console.log("incorrect Password");
+                        res.redirect(`/profile/${req.params.id}`);
+                    }
+                });
+            });
+    } else {
+        knex('profile').where('id', req.params.id).update({
+            first_name: req.body.first_name,
+            last_name: req.body.last_name,
+            email: req.body.email
+        }).then(data => {
+            res.redirect(`/profile/${req.params.id}`)
+        });
+    }
+
+});
 
 router.delete('/:id/delete', function(req, res, next) {
     knex('profile').where('id', req.params.id).first().del().then(data => {
         res.redirect('/admin');
     });
-=======
-=======
->>>>>>> 9c13a62c214cd336742eeac34166c387b6ccc041
-router.post('/:id/update', function(req, res, next){
-  let profile = req.body;
-  console.log(profile);
-  if (profile.password.length>0){
-    knex.select().table('profile').where('id', req.params.id).first()
-      .then(data=>{
-        protect.decrypt(data.password, profile.password).then(result=>{
-          if (result){
-            console.log("password is correct")
-            if ((profile.new_password === profile.new_password_verify)&&(profile.new_password.length>7)){
-              console.log("passwords match")
-              req.body.new_password = protect.encrypt(req.body.new_password).then(data=>{
-                knex('profile').where('id', req.params.id).update({
-                  first_name: profile.first_name,
-                  last_name: profile.last_name,
-                  email: profile.email,
-                  password: data
-                }).then(data=>{
-                  res.redirect(`/profile/${req.params.id}`);
-                });
-              })
-            } else {
-              console.log("new passwords do not match or not long enough.")
-              res.redirect(`/profile/${req.params.id}`);
-            }
-          } else {
-            console.log("incorrect Password");
-            res.redirect(`/profile/${req.params.id}`);
-          }
-        });
-      });
-  } else {
-    knex('profile').where('id', req.params.id).update({
-      first_name: req.body.first_name,
-      last_name: req.body.last_name,
-      email: req.body.email
-    }).then(data=>{
-      res.redirect(`/profile/${req.params.id}`)
-    });
-  }
-
-});
-
-router.delete('/:id/delete', function(req, res, next) {
-   knex('profile').where('id', req.params.id).first().del().then(data => {
-       res.redirect('/admin');
-   });
-<<<<<<< HEAD
->>>>>>> f715b3deaba56d24f2bae76f269f77b93a3ea92f
-=======
->>>>>>> 9c13a62c214cd336742eeac34166c387b6ccc041
 });
 
 module.exports = router;
