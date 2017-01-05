61df762e55ab26c173a227c7d07eba6118709792
Tyler Briskie <tylerbriskie@gmail.com>
2017-01-05 12:17:17 -0700

    merging development

--- routes/profile.js
+++ routes/profile.js
@@ -13,7 +13,30 @@ router.get('/:id', function(req, res, next) {
 
 router.post('/:id/update', function(req, res, next){
   let profile = req.body;
-  console.log(profile);
+  console.log(profile.password);
+  if (profile.password.length>0){
+    knex.select().table('profile').where('email', profile.email).first()
+      .then(data=>{
+        protect.decrypt(data.password, profile.password).then(result=>{
+          if (result){
+            console.log("password correct")
+            if (profile.new_password === profile.new_password_verify){
+              console.log("passwords match")
+              knex('profile').where('id', req.params.id).update({
+                first_name: profile.first_name,
+                last_name: profile.last_name,
+                email: profile.email,
+                password: profile.new_password
+              }).then(data=>{
+                res.redirect(`/profile/${req.params.id}`);
+              });
+            }
+          } else {
+            console.log("passwords do not match")
+          }
+        });
+      });
+  } 
   knex.select().table('profile').where('email', profile.email).first()
     .then(data=>{
       protect.decrypt(data.password, profile.password).then(result=>{
@@ -41,7 +64,7 @@ router.post('/:id/update', function(req, res, next){
   }).then(data=>{
     res.redirect(`/profile/${req.params.id}`)
   });
-})
+});
 
 router.delete('/:id/delete', function(req,res,next) {
   knex('profile').where('id', req.params.id).first().del().then(data=>{
