4c5eb74a81c905dd11547ef8e4291c43ac75ad78
Tyler Briskie <tylerbriskie@gmail.com>
2017-01-04 19:55:09 -0700

    delete route working on update profile page

--- routes/profile.js
+++ routes/profile.js
@@ -9,11 +9,19 @@ router.get('/:id', function(req, res, next) {
 
 });
 
-router.delete('/:id/delete', function(req,res,next) {
+router.put('/:id/update', function(req, res, next){
+  console.log(req.params.id)
+  knex('profile').where('id', req.params.id).update({
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    email: req.body.email
+  })
+})
+
+router.post('/:id/delete', function(req,res,next) {
   knex('profile').where('id', req.params.id).first().del().then(data=>{
     res.redirect('/');
-  }
-  );
+  });
 });
 
 module.exports = router;
