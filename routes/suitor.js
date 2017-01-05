39cfffa7c7f299bc1fb472d8cec264dd69e48528
Darren Hankins <hankins.darren@gmail.com>
2017-01-05 16:16:21 -0700

    updated suitor.js get interests

--- routes/suitor.js
+++ routes/suitor.js
@@ -4,46 +4,35 @@ var knex = require('../db/knex.js');
 var suitor = require('../db/suitor.js');
 /* GET users listing. */
 router.get('/:id', function(req, res, next) {
-  knex('suitor').where('id', req.params.id).first().then(data=>{
-    res.render('suitor', {data});
-  });
-  // knex('suitor_interest')
-  // .where('suitor_id', req.params.id)
-  // .then(data2=>{
-  //   console.log("suitor_interests - "+data2[0]);
-  //   var interest_id = data2[0].interest_id;
-  //   knex('interest')
-  //   .where('id', interest_id)
-  //   .then(data3=>{
-  //     console.log(data3);
-  //   })
-  // })
-
-  knex.from('suitor')
-    .innerJoin('suitor_interest', 'suitor.id', 'suitor_interest.suitor_id')
-    .then(data4 => {
-      console.log(data4);
+    knex('suitor').where('id', req.params.id).first().then(suitor => {
+        suitor.interests = [];
+        knex('suitor_interest')
+            .where('suitor_id', req.params.id)
+            .then(suitor_interests => {
+                let interests = suitor_interests.map((item) => {
+                    return knex('interest')
+                        .where('id', item.interest_id).then((singleInterest) => {
+                            let interest = {
+                                id: singleInterest[0].id,
+                                name: singleInterest[0].interest_name
+                            };
+                            suitor.interests.push(interest);
+                        })
+                })
+                return Promise.all(interests)
+            }).then(info => {
+                console.log(suitor);
+                res.render('suitor', {
+                    data: suitor
+                })
+            });
     });
-
-//   knex('suitor')
-//   .select(['projects.id', 'projects.project_number', 'projects.project_name', 'projects.start_date', 'projects.end_date',
-//     'owners.name', 'owners.email_addr as owner_email_addr',
-//     'clients.company', 'clients.email_addr as client_email_addr', 'clients.first_name', 'clients.last_name'])
-// .innerJoin('owners', 'owners.id', 'projects.owner_id')
-// .innerJoin('clients', 'clients.id', 'projects.client_id');
-  // suitor_interest
-  // suitor_id
-  // interest_id
-
-  // id
-  // interest_name
 });
 
-router.delete('/:id/delete', function(req,res,next) {
-  knex('suitor').where('id', req.params.id).first().del().then(data=>{
-    res.redirect('/');
-  }
-  );
+router.delete('/:id/delete', function(req, res, next) {
+    knex('suitor').where('id', req.params.id).first().del().then(data => {
+        res.redirect('/');
+    });
 });
 
 // router.get('/:id/suitor', (req,res) => {
