8d1906af4a3980f58d5281cf8073be21c4e69b2e
Tyler Briskie <tylerbriskie@gmail.com>
2017-01-05 09:17:35 -0700

    added user delete and update of profile page

--- routes/profile.js
+++ routes/profile.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var knex = require('../db/knex.js');
+var protect = require('../db/encryption.js');
+
 /* GET users listing. */
 router.get('/:id', function(req, res, next) {
   knex('profile').where('id', req.params.id).first().then(data=>{
@@ -9,13 +11,18 @@ router.get('/:id', function(req, res, next) {
 
 });
 
-router.put('/:id/update', function(req, res, next){
-  console.log(req.params.id)
+router.post('/:id/update', function(req, res, next){
+  let userPassword = protect.encrypt(req.body.password).then(data=>{
+    return data;
+  });
+  console.log(userPassword);
   knex('profile').where('id', req.params.id).update({
     first_name: req.body.first_name,
     last_name: req.body.last_name,
     email: req.body.email
-  })
+  }).then(data=>{
+    res.redirect(`/profile/${req.params.id}`)
+  });
 })
 
 router.post('/:id/delete', function(req,res,next) {
