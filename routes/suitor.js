a1a0e56df197c83d7dd00a5b2077e9565dec126c
“Andrew <abiv23@gmail.com>
2017-01-06 11:37:24 -0700

    toolkit start

--- routes/suitor.js
+++ routes/suitor.js
@@ -4,23 +4,24 @@ var knex = require('../db/knex.js');
 var suitor = require('../db/suitor.js');
 /* GET users listing. */
 router.get('/:id', function(req, res, next) {
-  knex('suitor').where('id', req.params.id).first().then(data=>{
-    res.render('suitor', {data});
-  })
+    knex('suitor').where('id', req.params.id).first().then(data => {
+        res.render('suitor', {
+            data
+        });
+    })
 
 });
 
-router.delete('/:id/delete', function(req,res,next) {
-  knex('suitor').where('id', req.params.id).first().del().then(data=>{
-    res.redirect('/');
-  }
-  );
+router.delete('/:id/delete', function(req, res, next) {
+    knex('suitor').where('id', req.params.id).first().del().then(data => {
+        res.redirect('/');
+    });
 });
 
-router.get('/:id/suitor', (req,res) => {
-  if(!isNaN(req.params.id)) {
+router.get('/:id/suitor', (req, res) => {
+    if (!isNaN(req.params.id)) {
 
-  }
+    }
 });
 
 module.exports = router;
