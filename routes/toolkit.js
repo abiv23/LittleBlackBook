dddae261728438a1b144d75091e1415e1c2833a3
“Andrew <abiv23@gmail.com>
2017-01-06 15:46:52 -0700

    :tada: toolkit complete

--- routes/toolkit.js
+++ routes/toolkit.js
@@ -15,28 +15,26 @@ router.get('/sos', function(req, res, next) {
     });
 });
 
-router.get('/common/:suitor_id', function(req, res, next) {
-    //get user_id from signed cookie
-    //get suitor_id from params
-    //querry the interest table where the user_id and suitor_id match
-    //select * from user_interests where user_id is req.params.id (join to interest table)
-    //select * from user_interests where user_id is req.params.id (join to interest table)
-    //iterate (for loop) over (javascript find common values in two arrays)
-    //then render common view and append in interests
-    res.render('common');
-});
+// router.get('/common/:suitor_id', function(req, res, next) {
+//     //get user_id from signed cookie
+//     //get suitor_id from params
+//     //querry the interest table where the user_id and suitor_id match
+//     //select * from user_interests where user_id is req.params.id (join to interest table)
+//     //select * from user_interests where user_id is req.params.id (join to interest table)
+//     //iterate (for loop) over (javascript find common values in two arrays)
+//     //then render common view and append in interests
+//     res.render('common');
+// });
 
 router.get('/their/:suitor_id', (req, res) => {
     //use suitor_id from req.params.suitor_id to find it's matching interest_id in the suitor_interest table
     //then use that matching interest_id to look up the interest.name in the interest table
     //now need a join to pull interest by interest_id from the interests table
-
     knex.select('*')
         .from('suitor_interest')
         .where('suitor_id', req.params.suitor_id)
-        .join('interest', 'suitor_interest.suitor_id', 'interest.id')
+        .join('interest', 'suitor_interest.interest_id', 'interest.id')
         .then((suitor_interest) => {
-            console.log(suitor_interest);
             res.render('toolkit-their-interests', {
                 suitor_interest
             });
