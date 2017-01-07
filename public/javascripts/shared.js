edc3a3e0f4f5443f241840a0d7050980ad00b0b4
Tyler Briskie <tylerbriskie@gmail.com>
2017-01-07 15:25:46 -0700

    fixed center justify on stars for plan a date view

--- public/javascripts/shared.js
+++ public/javascripts/shared.js
@@ -45,9 +45,9 @@ $('.suitor_select').change(()=> {
     $('.contact_container').empty();
     $('.contact_container').hide();
     $('.contact_container').append(`<img class='contact' src='${suitor.image_url}'/>`);
+    $('.contact_container').append(`<span class="stars center">${suitor.rating}</span>`);
     $('.contact_container').append(`<p class='contact-name'><span class='name'><b>${suitor.name}</b></span></p>`);
     $('.contact_container').append(`<p class='contact-met'><b>Where Met?</b> ${suitor.where_met}</p>`);
-    $('.contact_container').append(`<span class="stars center">${suitor.rating}</span>`);
     $('.contact_container').append(`<p class='contact-note'><b>Note: </b> ${suitor.note}</p>`);
     $('.contact_container').fadeIn("slow");
     console.log(suitor);
