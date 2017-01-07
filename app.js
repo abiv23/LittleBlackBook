257aa2db531e929fc3c9e603fd9047e87577d9b3
“Andrew <abiv23@gmail.com>
2017-01-07 14:43:36 -0700

    review date view

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ var toolkit = require('./routes/toolkit');
 var logout = require('./routes/logout')
 var auth = require('./auth/auth.js');
 var plan_date = require('./routes/plan_date')
+var review_date = require('./routes/review_date')
 var app = express();
 
 
@@ -40,9 +41,10 @@ app.use('/login', login);
 app.use('/admin', auth.adminOnly, admin);
 app.use('/signup', signup);
 app.use('/logout', logout);
-app.use('/contacts', auth.ensureLoggedIn, auth.setUser, contacts)
-app.use('/toolkit', auth.ensureLoggedIn ,toolkit);
-app.use('/plan_date', auth.ensureLoggedIn, plan_date)
+app.use('/contacts', auth.ensureLoggedIn, auth.setUser, contacts);
+app.use('/toolkit', auth.ensureLoggedIn, toolkit);
+app.use('/plan_date', auth.ensureLoggedIn, plan_date);
+app.use('/review_date', review_date);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
--- seeds/06date.js
+++ seeds/06date.js
@@ -3,30 +3,34 @@ exports.seed = function(knex, Promise) {
     return knex.raw('DELETE FROM date; ALTER SEQUENCE date_id_seq RESTART WITH 5')
         .then(() => {
             const dates = [{
+                id: 1,
                 suitor_id: 1,
                 profile_id: 1,
-                date: '2017-1-2',
+                date: '01/04/2017',
                 time: '01:00:00',
                 location: 'Chucky Cheese',
                 rating: 10
             }, {
+                id: 2,
                 suitor_id: 2,
                 profile_id: 3,
-                date: '2017-1-2',
+                date: '01/02/2017',
                 time: '01:00:00',
                 location: 'Linger',
                 rating: 10
             }, {
+                id: 3,
                 suitor_id: 3,
                 profile_id: 2,
-                date: '2017-1-2',
+                date: '01/04/2017',
                 time: '01:00:00',
                 location: '7-11',
                 rating: 10
             }, {
+                id: 4,
                 suitor_id: 4,
                 profile_id: 4,
-                date: '2017-1-2',
+                date: '01/05/2017',
                 time: '01:00:00',
                 location: 'Colt and Grey',
                 rating: 10
