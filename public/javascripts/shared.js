a090c4e59dc69071540a8cd0b5adca33088d65de
Chris Houdlette <choudlette@gmail.com>
2017-01-07 14:41:59 -0700

    merging

--- public/javascripts/shared.js
+++ public/javascripts/shared.js
@@ -21,6 +21,20 @@ $.get(`http://localhost:3000/plan_date/get_contact/${id}`)
   $('.contact_container').append(`<p class='contact-note'><b>Note: </b> ${suitor.note}</p>`);
   $('.contact_container').fadeIn("slow");
   console.log(suitor);
+
+  $.fn.stars = function() {
+  return $(this).each(function() {
+      var val = parseFloat($(this).html());
+      halfVal = (val/2);
+      var size = Math.max(0, (Math.min(5, halfVal))) * 15;
+      var $span = $('<span />').width(size);
+      $(this).html($span);
+  });
+  };
+  $(function() {
+    $('span.stars').stars();
+  });
+
 })
 
 
@@ -33,10 +47,26 @@ $('.suitor_select').change(()=> {
     $('.contact_container').append(`<img class='contact' src='${suitor.image_url}'/>`);
     $('.contact_container').append(`<p class='contact-name'><span class='name'><b>${suitor.name}</b></span></p>`);
     $('.contact_container').append(`<p class='contact-met'><b>Where Met?</b> ${suitor.where_met}</p>`);
-    $('.contact_container').append(`<p class='contact-rating'><b>Rating:</b> ${suitor.rating}</p>`);
+    $('.contact_container').append(`<span class="stars center">${suitor.rating}</span>`);
     $('.contact_container').append(`<p class='contact-note'><b>Note: </b> ${suitor.note}</p>`);
     $('.contact_container').fadeIn("slow");
     console.log(suitor);
+
+    $.fn.stars = function() {
+    return $(this).each(function() {
+        var val = parseFloat($(this).html());
+        halfVal = (val/2);
+        var size = Math.max(0, (Math.min(5, halfVal))) * 15;
+        var $span = $('<span />').width(size);
+        $(this).html($span);
+    });
+    };
+    $(function() {
+      $('span.stars').stars();
+    });
+
+
+
   })
 }
 );
