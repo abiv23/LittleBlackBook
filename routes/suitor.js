0bba45f10f59e0782fe88f12e50d09000e697062
Darren Hankins <hankins.darren@gmail.com>
2017-01-06 17:47:40 -0700

    fixed suitor interest added and deletion

--- routes/suitor.js
+++ routes/suitor.js
@@ -4,24 +4,22 @@ var knex = require('../db/knex.js');
 var suitor = require('../db/suitor.js');
 var auth = require('../auth/auth.js');
 
-
-
 /* GET users listing. */
 // router.get('/:id', auth.allowAccess, function(req, res, next) {
 router.get('/:id', function(req, res, next) {
     knex('suitor').where('id', req.params.id).first().then(suitor => {
         suitor.interests = [];
-        suitor.allInterests= [];
+        suitor.allInterests = [];
         knex.select('*').from('interest').then(allInterests => {
-          // let allInterest = {
-          //     id: allInterests[0].id,
-          //     name: allInterests[0].interest_name
-          // };
-          suitor.allInterests.push(allInterests);
-          console.log(suitor.allInterests[0][1].interest_name);
-          // console.log(suitor.allInterests.interest_name);
+            // let allInterest = {
+            //     id: allInterests[0].id,
+            //     name: allInterests[0].interest_name
+            // };
+            suitor.allInterests.push(allInterests);
+            console.log(suitor.allInterests[0][1].interest_name);
+            // console.log(suitor.allInterests.interest_name);
         }).then(
-        knex('suitor_interest')
+            knex('suitor_interest')
             .where('suitor_id', req.params.id)
             .then(suitor_interests => {
                 let interests = suitor_interests.map((item) => {
@@ -36,98 +34,83 @@ router.get('/:id', function(req, res, next) {
                 })
                 return Promise.all(interests)
             }).then(info => {
-                for (var j = 0; j<suitor.interests.length; j++){
-                  for (var i = 0; i<suitor.allInterests[0].length; i++){
-                    if (suitor.allInterests[0][i].id == suitor.interests[j].id) {
-                      suitor.allInterests[0][i].interest_active = 1;
+                for (var j = 0; j < suitor.interests.length; j++) {
+                    for (var i = 0; i < suitor.allInterests[0].length; i++) {
+                        if (suitor.allInterests[0][i].id == suitor.interests[j].id) {
+                            suitor.allInterests[0][i].interest_active = 1;
+                        }
                     }
-                  }
                 }
                 res.render('suitor', {
                     data: suitor
                 })
             })
-          )
+        )
     });
 });
 
 
-router.post('/:id/update', auth.allowAccess, function(req, res, next){
-  // let profile = req.body;
-  console.log("length = "+req.body.suitor_interests.length);
+router.post('/:id/update', auth.allowAccess, function(req, res, next) {
+    // console.log("suitor_interests = "+req.body.suitor_interests);
 
-        // delete all suitor interests
-        // knex('suitor_interest')
-        //   .where('suitor_id', req.params.id)
-        //   .del()
-        //   .then();
-          // .then(data => { });
+    // delete all suitor interests
+    knex('suitor_interest')
+        .where('suitor_id', req.params.id)
+        .del()
+        .then(data => {
+            if (!req.body.suitor_interests) {} else if (req.body.suitor_interests.length == 1) {
+                return knex('suitor_interest')
+                    .insert({
+                        suitor_id: req.params.id,
+                        interest_id: req.body.suitor_interests
+                    })
+                    //next();
+            } else {
+                console.log("suitor_interests = " + req.body.suitor_interests);
+                console.log("d1 = " + data);
+                let s = req.body.suitor_interests.map((interest) => {
+                    return knex('suitor_interest')
+                        .insert({
+                            suitor_id: req.params.id,
+                            interest_id: interest
+                        })
+                });
+                return Promise.all(s);
+            }
+        })
+        .then(data => {
+            console.log("d = " + data);
+            res.redirect(`/suitor/${req.params.id}`);
+        });
 
-          // let interests = req.body.suitor_interests.map((item) => {
-          //     console.log("interests = "+interests);
-          // });
-
-        // add suitor interests
-          knex('suitor_interest')
-            // .where('suitor_id', req.params.id)
-            // .update(suitor_interest => {
-            .update({
-                // let interests = suitor_interest.map((item) => {
-                  suitor_id: req.params.id,
-                  interest_id: req.body.suitor_interests
-                // })
-                // return Promise.all(suitor_interests)
-            })
-            .then(data=>{
-              res.redirect(`/suitor/${req.params.id}`);
-            });
-
-            // update suitor profile
-            // knex('suitor')
-            //   .where('id', req.params.id)
-            //   .update({
-            //   name: req.body.name,
-            //   age: req.body.age,
-            //   where_met: req.body.where_met,
-            //   rating: req.body.rating
-            // }).then(data=>{
-            //   res.redirect(`/suitor/${req.params.id}`);
-            // });
-
+    // re-insert suitor interests
+    // req.body.suitor_interests.forEach((interest)=>{
+    //   knex('suitor_interest')
+    //     .insert({
+    //       suitor_id: req.params.id,
+    //       interest_id: interest
+    //     })
+    //     .then(data=>{
+    //       res.redirect(`/suitor/${req.params.id}`);
+    //     });
+    // });
 });
 
-router.post('/:id/updatePhoto', auth.allowAccess, function(req, res, next){
-  console.log(req.body.photo_url)
-  knex('profile').where('id', req.params.id).update({
-    image_url: req.body.photo_url
-  }).then(data=>{
-    res.redirect(`/profile/${req.params.id}`);
-  })
-
-});
-
-router.delete('/:id/delete', auth.allowAccess,function(req, res, next) {
-   knex('profile').where('id', req.params.id).first().del().then(data => {
-      res.clearCookie('user_id');
-      res.redirect('/');
-   });
-});
-
-
-
-
-// router.get('/:id/suitor', (req,res) => {
-//   if(!isNaN(req.params.id)) {
-//     Suitor.getByUser(req.params.id)
-//     // .then(suitors => {
-//     //   if (suitors.length > 1) {
-//     //     res.json(suitors);
-//     //   } else {
-//     //     res.status(400);
-//     //     res.json(suitors);
-//     //   }
-//     });
-//   }
+// router.post('/:id/updatePhoto', auth.allowAccess, function(req, res, next){
+//   console.log(req.body.photo_url)
+//   knex('profile').where('id', req.params.id).update({
+//     image_url: req.body.photo_url
+//   }).then(data=>{
+//     res.redirect(`/profile/${req.params.id}`);
+//   })
 // });
+
+// router.delete('/:id/delete', auth.allowAccess,function(req, res, next) {
+//    knex('profile').where('id', req.params.id).first().del().then(data => {
+//       res.clearCookie('user_id');
+//       res.redirect('/');
+//    });
+// });
+
 
 module.exports = router;
